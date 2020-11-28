620ff7844eaf0fddfd6ff4de85eb6ee4e81d5d47
Seth Mitchell <sayotte.mitchell@gmail.com>
2020-11-28 11:39:36 -0500

    Contact Page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,55 @@
 import React from "react";
 
+import githubIcon from "../assets/github-icon.png";
+import linkedinIcon from "../assets/linkedin-icon.png";
+
 const Contact = () => {
     return (
         <React.Fragment>
         <section className="pageContainer">
           <h1 className="pageHeader">Contact</h1>
+
+          <p id="connect-copy">Feel free to check out my GitHub & LinkedIn for additional details about my work, or get in touch through email.</p>
+
+          <div>
+            <div>
+              < a href="https://github.com/sethayotte" target="_blank">
+              <img className="socialConnectIcon" src={githubIcon}/>
+              </a>
+              <a href="https://www.linkedin.com/in/sethayottemitchell/" target="_blank">
+              <img className="socialConnectIcon" src={linkedinIcon}/>
+              </a>
+            </div>
+            <div>
+              <h2 className="socialConnectButton">Send an Email</h2>
+            </div>
+          </div>
+        
+          {/* <div id="connect-copy">
+    <p>Feel free to check out my GitHub & LinkedIn for additional details about my work, or get in touch through email.</p>
+  </div>
+
+  <div className="connect-blocks">
+    <div className="social-buttons" id="connect-social-buttons">
+      <div>
+        <a className="social-icon mr-4" href="https://github.com/sethayotte" target="_blank">
+          <img src="assets/github-icon.png" height="30" className="d-inline-block align-center" alt="">
+        </a>
+      </div>
+
+      <div>
+        <a className="social-icon ml-4" href="https://www.linkedin.com/in/sethayottemitchell/" target="_blank">
+          <img src="assets/linkedin-icon.png" height="30" className="d-inline-block align-center" alt="">
+        </a>
+      </div>
+    </div>
+
+    <div className="email-button">
+      <a href="mailto:sayotte.mitchell@gmail.com?subject=Howdy!">
+        <h2>send an email</h2>
+      </a>
+    </div>
+  </div> */}
         </section>
       </React.Fragment>
     );
--- src/components/SocialNavBar.js
+++ src/components/SocialNavBar.js
@@ -1,20 +1,13 @@
-import React, { useState } from 'react';
-import {
-  Navbar,
-  NavItem,
-} from 'reactstrap';
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route
-  } from "react-router-dom";
+import React, { useState } from "react";
+import { Navbar, NavItem } from "reactstrap";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import connectIcon from "../assets/connect-icon.png";
 import githubIcon from "../assets/github-icon.png";
 import linkedinIcon from "../assets/linkedin-icon.png";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-import Contact from './Contact';
+import Contact from "./Contact";
 
 const SocialNavBar = (props) => {
   const [isOpen, setIsOpen] = useState(false);
@@ -23,31 +16,32 @@ const SocialNavBar = (props) => {
 
   return (
     <div>
-        <Router>
-      <Navbar className="navbar navSocialContainer" fixed='bottom'>
-      <NavItem className="socialNavItem">
-      <a href="https://github.com/sethayotte" target="_blank">
+      <Router>
+        <Navbar className="navbar navSocialContainer" fixed="bottom">
+          <NavItem className="socialNavItem">
+            <a href="https://github.com/sethayotte" target="_blank">
               <img src={githubIcon} className="socialNavLinks" />
-              </a>
-            </NavItem>
-            <NavItem className="socialNavItem">
-                <a href="https://www.linkedin.com/in/sethayottemitchell/" target="_blank">
+            </a>
+          </NavItem>
+          <NavItem className="socialNavItem">
+            <a
+              href="https://www.linkedin.com/in/sethayottemitchell/"
+              target="_blank"
+            >
               <img src={linkedinIcon} className="socialNavLinks" />
-              </a>
-            </NavItem>
-            <NavItem className="socialNavItem">
-                <a href='/contact'>
+            </a>
+          </NavItem>
+          <NavItem className="socialNavItem">
+            <a href="/contact">
               <img src={connectIcon} className="socialNavLinks" />
-              </a>
-            </NavItem>
-      </Navbar>
+            </a>
+          </NavItem>
+        </Navbar>
 
-      <Switch>
-         
-      </Switch>
+        <Switch></Switch>
       </Router>
     </div>
   );
-}
+};
 
 export default SocialNavBar;
