b33f89839fd1cfd4c559e7c69113277a97556651
Seth Mitchell <sayotte.mitchell@gmail.com>
2020-11-28 11:10:13 -0500

    Social Nav Styling & Link Hookup

--- src/components/SocialNavBar.js
+++ src/components/SocialNavBar.js
@@ -1,26 +1,20 @@
 import React, { useState } from 'react';
 import {
-  Collapse,
   Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
   NavItem,
-  NavLink
 } from 'reactstrap';
 import {
     BrowserRouter as Router,
     Switch,
     Route
   } from "react-router-dom";
-import navIcon from '../assets/SM2021.png';
+
+import connectIcon from "../assets/connect-icon.png";
+import githubIcon from "../assets/github-icon.png";
+import linkedinIcon from "../assets/linkedin-icon.png";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-import Projects from './Projects';
 import Contact from './Contact';
-import Resume from './Resume';
-import Landing from './Landing';
-import EverydayPortfolio from './Projects/EverydayPortfolio';
 
 const SocialNavBar = (props) => {
   const [isOpen, setIsOpen] = useState(false);
@@ -30,8 +24,22 @@ const SocialNavBar = (props) => {
   return (
     <div>
         <Router>
-      <Navbar className="navbar navSocialContainer" expand="md" fixed='bottom'>
-        <NavbarBrand href="/" className="navIcon"><img src={navIcon} id="navIcon" /></NavbarBrand>
+      <Navbar className="navbar navSocialContainer" fixed='bottom'>
+      <NavItem className="socialNavItem">
+      <a href="https://github.com/sethayotte" target="_blank">
+              <img src={githubIcon} className="socialNavLinks" />
+              </a>
+            </NavItem>
+            <NavItem className="socialNavItem">
+                <a href="https://www.linkedin.com/in/sethayottemitchell/" target="_blank">
+              <img src={linkedinIcon} className="socialNavLinks" />
+              </a>
+            </NavItem>
+            <NavItem className="socialNavItem">
+                <a href='/contact'>
+              <img src={connectIcon} className="socialNavLinks" />
+              </a>
+            </NavItem>
       </Navbar>
 
       <Switch>
