da0c27de09d4e15492176160bd29506fb05114b4
Seth Mitchell <sayotte.mitchell@gmail.com>
2021-01-01 10:18:19 -0500

    Mesh Project Detail Elements

--- src/components/Projects/Mesh.js
+++ src/components/Projects/Mesh.js
@@ -2,7 +2,8 @@ import React from "react";
 import { Link, animateScroll as scroll } from "react-scroll";
 
 import SproutIntro from '../../assets/Sprout_Mobile.png';
-import SproutHero from '../../assets/sprout_hero.png';
+import MeshHero from '../../assets/MeshMockup.png';
+import MeshMobileHero from '../../assets/MeshMobileHero.png';
 import SproutTab1 from '../../assets/sprout_tablet.png';
 import SproutTab2 from '../../assets/sprout_tablet2.png';
 
@@ -13,11 +14,12 @@ const Sprout = () => {
       <section className="pageContainer" id="meshPage">
         <section id="meshHeader">
         <h1 className="projectHeader" id="mesh">Mesh</h1>
-        <img id="meshHero" src={SproutHero} alt="Sprout Hero Mockup" />
+        <img id="meshHero" src={MeshHero} alt="Sprout Hero Mockup" />
+        <img id="meshMobileHero" src={MeshMobileHero} alt="Sprout Hero Mockup" />
         </section>
 
-        <section id="sprout-tagline-resources">
-        <h2 id="sprout-tagline">Reimagining talent in<br />the wake of COVID-19.</h2>
+        <section id="mesh-tagline-resources">
+        <h2 id="mesh-tagline">A collaborative space for creatives.<br />Prototype created at VCU.</h2>
         <section id="document-container">
         <a href="https://github.com/sethayotte/" target="_blank" rel="noreferrer">
         <div>
@@ -25,7 +27,7 @@ const Sprout = () => {
         </div>
         </a>
         <Link
-            to="sproutProject"
+            to="meshProject"
             smooth={true}
             duration={800}
         >
@@ -36,45 +38,63 @@ const Sprout = () => {
         </section>
         </section>
 
-        <section className="projectDescription" id="sproutProject">
+        <section className="projectDescription" id="meshProject">
             
-            <div id="introContainer">
-                <div>
-                <img id="sproutIntro" src={SproutIntro} alt="Sprout Intro Mobile Mockup" />
-                </div>
-                <div id="sproutCopy">
-                    <h4 className="descriptionSubheading">About the Sprout Project</h4>
-                    <p>
-                    With a passion for cars at a young age, I absolutely love driving and will always take the long winding road home if I can help it.  Each car I’ve owned has run with me through significant moments in my life and I wanted a place to hold a record of these vehicles and the hundreds of thousands of miles they’ve taken me.<br /><br />  
+            <div id="projectContainer">
+                <div id="item1">
+                    <div>
+                    <img id="meshIntro" src={SproutIntro} alt="Sprout Intro Mobile Mockup" />
+                    </div>
+                    <div id="meshCopy">
+                        <h4 className="descriptionSubheading">About the Sprout Project</h4>
+                        <p>
+                        With a passion for cars at a young age, I absolutely love driving and will always take the long winding road home if I can help it.  Each car I’ve owned has run with me through significant moments in my life and I wanted a place to hold a record of these vehicles and the hundreds of thousands of miles they’ve taken me.<br /><br />  
 
-Seth’s Garage will be an archive of every car I’ve owned with basic specifications and mileage driven.  Down the road, I plan on implementing an OBD integration that will track mileage as I cruise along and will relay back to the site to show total miles driven during the collective lifetime of the fleet.
-                    </p>
+                        Seth’s Garage will be an archive of every car I’ve owned with basic specifications and mileage driven.  Down the road, I plan on implementing an OBD integration that will track mileage as I cruise along and will relay back to the site to show total miles driven during the collective lifetime of the fleet.
+                        </p>
+                    </div>
+                </div>
+                <div id="item2">
+                    <div id="meshCopy">
+                        <h4 className="descriptionSubheading">About the Sprout Project</h4>
+                        <p>
+                        With a passion for cars at a young age, I absolutely love driving and will always take the long winding road home if I can help it.  Each car I’ve owned has run with me through significant moments in my life and I wanted a place to hold a record of these vehicles and the hundreds of thousands of miles they’ve taken me.<br /><br />  
+
+                        Seth’s Garage will be an archive of every car I’ve owned with basic specifications and mileage driven.  Down the road, I plan on implementing an OBD integration that will track mileage as I cruise along and will relay back to the site to show total miles driven during the collective lifetime of the fleet.
+                        </p>
+                    </div>
+                    <div>
+                        <img id="meshIntro" src={SproutIntro} alt="Sprout Intro Mobile Mockup" />
+                    </div>
+                </div>
+                <div id="item3">
+                    <div>
+                        <img id="meshIntro" src={SproutIntro} alt="Sprout Intro Mobile Mockup" />
+                    </div>
+                    <div id="meshCopy">
+                        <h4 className="descriptionSubheading">About the Sprout Project</h4>
+                        <p>
+                        With a passion for cars at a young age, I absolutely love driving and will always take the long winding road home if I can help it.  Each car I’ve owned has run with me through significant moments in my life and I wanted a place to hold a record of these vehicles and the hundreds of thousands of miles they’ve taken me.<br /><br />  
+
+                        Seth’s Garage will be an archive of every car I’ve owned with basic specifications and mileage driven.  Down the road, I plan on implementing an OBD integration that will track mileage as I cruise along and will relay back to the site to show total miles driven during the collective lifetime of the fleet.
+                        </p>
+                    </div>
+                </div>
+                <div id="item4">
+                    <div id="meshCopy">
+                        <h4 className="descriptionSubheading">About the Sprout Project</h4>
+                        <p>
+                        With a passion for cars at a young age, I absolutely love driving and will always take the long winding road home if I can help it.  Each car I’ve owned has run with me through significant moments in my life and I wanted a place to hold a record of these vehicles and the hundreds of thousands of miles they’ve taken me.<br /><br />  
+
+                        Seth’s Garage will be an archive of every car I’ve owned with basic specifications and mileage driven.  Down the road, I plan on implementing an OBD integration that will track mileage as I cruise along and will relay back to the site to show total miles driven during the collective lifetime of the fleet.
+                        </p>
+                    </div>
+                    <div>
+                        <img id="meshIntro" src={SproutIntro} alt="Sprout Intro Mobile Mockup" />
+                    </div>
                 </div>
             </div>
 
-        </section>
-
-        <section>
-            <div className="design-codeBody">
-                <div className="sproutCol1">
-                    <div>
-                    <h4 className="projectSubheading">Cultivating my<br />development interest.</h4>
-                    <p>Lorem ipsum</p>
-                    </div>
-                    <div>
-                    <img src={SproutTab1} alt="Sprout Tablet Mockup 1" />  
-                    </div>
-                </div>
-                <div className="sproutCol2">
-                    <div>
-                    <img src={SproutTab2} alt="Sprout Tablet Mockup 2" />
-                    </div>
-                    <div>
-                    <h4 className="projectSubheading">A turning point for<br />design & code.</h4> 
-                    <p>Lorem ipsum</p>
-                    </div>
-                </div>
-            </div>
         </section>
 
         </section>
